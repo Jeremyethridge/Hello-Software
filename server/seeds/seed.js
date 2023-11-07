e031f64c0fb51db2e437c4a7573323aaa4bb69d5
Caine Winters@meNZ <caine.winters@outlook.com>
2023-11-06 20:12:36 -0500

    Some tutor prelim stuff

---
+++ server/seeds/seed.js
@@ -0,0 +1,14 @@
+const db = require('../config/connection');
+const cleanDB = require('./cleanDB');
+const { Tutors } = require('../models');
+
+const tutorsData = require('./tutorsData.json');
+
+db.once('open', async () => {
+  await cleanDB('Tutors', 'notSure');
+
+  await Tutors.insertMany(tutorsData);
+
+  console.log('Tutors seeded!');
+  process.exit(0);
+});
