a70e8cfe8061d225b49707ec73d4e6b7051251c5
HND <hans.doderlein99@gmail.com>
2023-11-06 21:26:28 -0500

    db colelction refresh

--- server/models/Tutors.js
+++ server/models/Tutors.js
@@ -1,4 +1,4 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const tutorSchema = new Schema({
   name: {
@@ -6,9 +6,7 @@ const tutorSchema = new Schema({
     required: true,
     unique: true,
   },
-  skill: {
-    type: String,
-  },
+  skill: [String],
   rate: {
     type: Number,
   },
@@ -16,10 +14,13 @@ const tutorSchema = new Schema({
     type: String,
     required: true,
     unique: true,
-    match: [/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please enter a valid email'],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please enter a valid email",
+    ],
   },
 });
 
-const Tutors = model('Tutors', tutorSchema);
+const Tutors = model("Tutors", tutorSchema);
 
 module.exports = Tutors;
--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,11 +1,11 @@
 const db = require("../config/connection");
-const cleanDB = require("./cleanDB");
+
 const { Tutors } = require("../models");
 
 const tutorsData = require("./tutorsData.json");
 
 db.once("open", async () => {
-  await cleanDB("Tutors", "Project3DB");
+  await db.dropCollection("tutors");
 
   await Tutors.insertMany(tutorsData);
 
