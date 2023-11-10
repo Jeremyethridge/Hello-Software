94fc38f5ee6b0ddc12131289bfe22e0fb5f9b7b9
HND <hans.doderlein99@gmail.com>
2023-11-09 20:07:42 -0500

    graphql setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,23 @@
 import { Outlet } from "react-router-dom";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <>
-      <nav>
-        <a href="/">Home</a>
-        <a href="/Login">Login</a>
-        <a href="/Signup">Signup</a>
-        <a href="/Tutors">Tutors</a>
-      </nav>
-      <Outlet />
+      <ApolloProvider client={client}>
+        <nav>
+          <a href="/">Home</a>
+          <a href="/Login">Login</a>
+          <a href="/Signup">Signup</a>
+          <a href="/Tutors">Tutors</a>
+        </nav>
+        <Outlet />
+      </ApolloProvider>
     </>
   );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { Home, Login, Signup, Tutors } from "./pages";
+import { Home, Tutors } from "./pages";
 
 const router = createBrowserRouter([
   {
@@ -11,8 +11,8 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       { index: true, element: <Home /> },
-      { path: "/Login", element: <Login /> },
-      { path: "/Signup", element: <Signup /> },
+      // { path: "/Login", element: <Login /> },
+      // { path: "/Signup", element: <Signup /> },
       { path: "/Tutors", element: <Tutors /> },
     ],
   },
