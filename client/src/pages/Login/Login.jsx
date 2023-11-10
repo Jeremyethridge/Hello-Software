a280ea252e34e6d711e4ac7f89af1092f77e0db7
James Weah <jweah@aggies.ncat.edu>
2023-11-09 21:26:14 -0500

    conditional rendering to signup page

---
+++ client/src/pages/Login/Login.jsx
@@ -0,0 +1,49 @@
+
+import '../Login/Login.css';
+
+export function Login()  {
+  return (
+    <div>
+      <div className="login-page">
+        <div className="border">
+          <div className="login-intro">
+            <div className='top-info'>
+              <h1 className="login-title">Login</h1>
+              <div className='toggles-login'>
+                <label className="switch">
+                  <input type="checkbox"></input>
+                  <span className="slider round"> Client</span>
+                </label>
+                <label className="switch">
+                  <input type="checkbox"></input>
+                  <span className="slider round"> Tutor</span>
+                </label>
+              </div>
+            </div>
+            <div>
+              <p className="get-started">to get started</p>
+            </div>
+          </div>
+          <div className="login-boxes">
+            <div>
+              <input className="email" placeholder="  Email"></input>
+            </div>
+            <div>
+              <input className="password" placeholder="  Password"></input>
+            </div>
+            <div>
+              <button>Continue</button>
+            </div>
+            <div>
+              <p className="change-page">
+                Do not have an account yet? <span className='here'> Register Here</span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
--- client/src/pages/Signup/Signup.jsx
+++ client/src/pages/Signup/Signup.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import { useState } from 'react';
 import '../Signup/Signup.css'
 
-const SignUp = () => {
+export function SignUp (){
+  const [clientIsChecked, setClientCheck] = useState(false);
+  const [tutorIsChecked, setTutorCheck] = useState(false)
+
+
+  const handleClient = (e) => {
+    setClientCheck(e.target.checked)
+  }
+  
+  const handleTutor = (e) => {
+    setTutorCheck(e.target.checked)
+  }
+
 
   return (
     <div>
@@ -10,11 +22,30 @@ const SignUp = () => {
           <div className="sign-up-intro">
             <div>
               <h1 className="signup-title">Signup</h1>
+              <div className="toggles-signup">
+                <label className="switch">
+                  <input
+                    checked={clientIsChecked}
+                    onChange={handleClient}
+                    type="checkbox"
+                  ></input>
+                  <span className="slider round"> Client</span>
+                </label>
+                <label className="switch">
+                  <input
+                  checked={tutorIsChecked}
+                  onChange={handleTutor}
+                    type="checkbox"
+                  ></input>
+                  <span className="slider round"> Tutor</span>
+                </label>
+              </div>
             </div>
             <div>
               <p className="get-started">to get started</p>
             </div>
           </div>
+
           <div className="sign-in-boxes">
             <div>
               <input className="user-name" placeholder="  Username"></input>
@@ -25,9 +56,37 @@ const SignUp = () => {
             <div>
               <input className="password" placeholder="  Password"></input>
             </div>
+            {tutorIsChecked ? (
+              <div>
+                <div>
+                  <input className="password" placeholder=" P Number" />
+                </div>
+                <div>
+                  <p className="skills-title">Skills</p>
+                  <div className="skills-box">
+                    <label>
+                      <input type="checkbox" /> HTML
+                    </label>
+                    <label>
+                      <input type="checkbox" /> CSS
+                    </label>
+                    <label>
+                      <input type="checkbox" /> JavaScript
+                    </label>
+                    <label>
+                      <input type="checkbox" /> React
+                    </label>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <></>
+            )}
+
             <div>
               <button>Continue</button>
             </div>
+
             <div>
               <p className="change-page">
                 Already registerd? <p>Login</p>
@@ -38,6 +97,5 @@ const SignUp = () => {
       </div>
     </div>
   );
-};
+}
 
-export default SignUp;
