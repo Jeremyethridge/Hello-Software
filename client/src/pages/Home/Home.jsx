9f8531c87a39fd82b1d6cc10305e5a3196640511
JeremyEthridge <Jeremyethridge6@yahoo.com>
2023-11-13 20:58:55 -0500

    added css to homepage, added data && data.getTutors, and aligned mainHeader and Navbar ;

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,8 +1,11 @@
+import '../Home/Home.css';
+
 export function Home() {
   return (
-    <>
+  <><div className='home-page'>
       <div className="mission">
         <h3>Mission statement:</h3>
+
         <p>
           Here at our tutoring center we aim to aid young developers in their
           journey to improve their coding skills. All current tutors are top
@@ -16,21 +19,23 @@ export function Home() {
           registered tutors.
         </p>
       </div>
-      <div className="login/signup">
+
+      <div className="login-signup">
         <p>
           Would you like to become a client and request tutor help? Click
-          <a href="/Signup">here</a> to register as a client!
+          <a href="/Signup"> here</a> to register as a client!
         </p>
 
         <p>
           Would you like to join our team and become our newest tutor? Click
-          <a href="/Signup">here</a> to join our ranks!
+          <a href="/Signup"> here</a> to join our ranks!
         </p>
 
         <p>
           Already a member? Click <a href="/Login">here</a> to login!
         </p>
       </div>
+    </div>
     </>
   );
 }
