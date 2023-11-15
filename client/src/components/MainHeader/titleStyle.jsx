30379dccf6d2480f416c32a97b009ab0b84b8397
James Weah <jweah@aggies.ncat.edu>
2023-11-14 21:32:53 -0500

    cards update

--- client/src/components/MainHeader/titleStyle.jsx
+++ client/src/components/MainHeader/titleStyle.jsx
@@ -3,15 +3,27 @@ import styled from "styled-components"
 
 export const CompanyName = styled.h1`
         display: flex;
-        text-align: center;
-        justify-content: center;
+        ${'' /* text-align: center;
+        justify-content: center; */}
         Font family: Tahoma,Verdana,Segoe,sans-serif;
-        justify-content: center;
+        ${'' /* justify-content: center; */}
+        justify-content: center;   
 `
 export const TopOScreen = styled.header`
-        position: fixed;
-        top: 0px;
-        width:100%;
-        max-width: 100%;
-        background: white;
-`
+  position: fixed;
+  top: 0px;
+  width: 100%;
+  max-width: 100%;
+  background: white;
+  ${'' /* display: flex;
+  flex-direction: column;
+        justify-content: center;
+        text-align: center; */}
+  ${'' /* @media screen and ((max-width: 570px )) {
+    TopOScreen {
+      width: 50%;
+    }
+  } */}
+`;
+
+
